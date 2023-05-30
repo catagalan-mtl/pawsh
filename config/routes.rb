0da5272da55738f256ee0941143b72764008a146
Catalina Galan <catalina.galan@hotmail.com>
2023-05-30 16:20:03 -0400

    view outfit new and simple_form added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
 
-  resources :outfits, only: [:index]
+  resources :outfits, only: [:index, :new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
