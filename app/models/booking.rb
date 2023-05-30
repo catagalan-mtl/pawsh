e2c7dc8782fc6c5801f98cf1c8c5e285ac3d9a05
Antoine Marchand <marchand41@gmail.com>
2023-05-30 14:45:07 -0400

    booking model and validations complete

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,9 +1,11 @@
 class Booking < ApplicationRecord
-  belongs_to :user
-  belongs_to :outfit
+  belongs_to :user, dependent: destroy
+  belongs_to :outfit, dependent: destroy
 
   STATUSES = %w[booked pending cancelled]
 
   validates :status, presence: true
   validates :status, inclusion: { in: STATUSES }
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
