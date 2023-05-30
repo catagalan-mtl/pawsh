716635a170101356d683827c49c6a94b3c0feb97
Catalina Galan <catalina.galan@hotmail.com>
2023-05-30 17:09:49 -0400

    added avatar_url validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :outfits
-  has_many :bookings
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :outfits
+  has_many :bookings
+
+  validates :avatar_url, presence: true
 end
