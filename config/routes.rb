8fef0c50b46e4fa4c8d4edb12e2475328035075b
Catalina Galan <catalina.galan@hotmail.com>
2023-05-30 13:48:40 -0400

    index route for outfit created

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+
+  resources :outfits, only: [:index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
