c7cd37c023b878300d3246caaca139da01ddc480
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-05-30 12:27:03 -0400

    added avatar_url to user, and to the seed

---
+++ db/migrate/20230530162239_add_avatarurl_to_user.rb
@@ -0,0 +1,5 @@
+class AddAvatarurlToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :avatar_url, :string
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@
 puts "Cleaning the users table"
 User.destroy_all
 puts "Generating 3 users"
-User.create!(email: "mary@exemple.com", first_name: "Mary", last_name: "Jane", password: "password")
-User.create!(email: "john@exemple.com", first_name: "John", last_name: "Smith", password: "password")
-User.create!(email: "johanna@exemple.com", first_name: "Johanna", last_name: "Lamontagne", password: "password")
+User.create!(email: "mary@exemple.com", first_name: "Mary", last_name: "Jane", password: "password", avatar_url: "https://res.cloudinary.com/dbgykplat/image/upload/v1685463153/panda_avatar.png")
+User.create!(email: "john@exemple.com", first_name: "John", last_name: "Smith", password: "password", avatar_url: "https://res.cloudinary.com/dbgykplat/image/upload/v1685463149/fox_avatar.png")
+User.create!(email: "johanna@exemple.com", first_name: "Johanna", last_name: "Lafont", password: "password", avatar_url: "https://res.cloudinary.com/dbgykplat/image/upload/v1685463145/chick_avatar.png")
 puts "All done"
