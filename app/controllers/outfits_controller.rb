4c449142a033ca71876b692b8e6a38453ed2bc26
Catalina Galan <catalina.galan@hotmail.com>
2023-05-30 15:57:59 -0400

    def new and def create are done

--- app/controllers/outfits_controller.rb
+++ app/controllers/outfits_controller.rb
@@ -4,4 +4,22 @@ class OutfitsController < ApplicationController
     @outfits = Outfit.all
   end
 
+  def new
+    @outfit = Outfit.new
+  end
+
+  def create
+    @outfit = Outfit.new(outfit_params)
+    if @outfit.save
+      redirect_to outfit_path(@outfit)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def outfit_params
+    params.require(:outfit).permit(:title, :size, :animal, :description, :occasion, :price_per_day)
+  end
 end
