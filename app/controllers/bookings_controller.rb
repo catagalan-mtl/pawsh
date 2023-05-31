ad3fad19dc662b78153d70066d64e9701374d181
Antoine Marchand <marchand41@gmail.com>
2023-05-31 14:30:38 -0400

    completed bookings new and create

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -13,6 +13,20 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.status = "pending"
+    @booking.outfit_id = params[:outfit_id]
+    @booking.user = current_user
+    if @booking.save
+      redirect_to bookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
