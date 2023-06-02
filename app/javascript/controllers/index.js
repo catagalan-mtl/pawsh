2335e5f06d28c525308f65b95ade01f8696d3bf3
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-06-02 10:47:02 -0400

    change according to kirstens comments

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,9 +4,6 @@
 
 import { application } from "./application"
 
-import BookingController from "./booking_controller"
-application.register("booking", BookingController)
-
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -9,9 +9,4 @@ class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
 
-  enum state: {
-    pending: 0,
-    accepted: 1,
-    declined: 2
-  }
 end
