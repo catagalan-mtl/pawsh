9ee7dae7aa47a00f765823d63be9c7b8dd73a410
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-07-08 05:24:23 -0400

    added clickable thumbnail, remain removing the hover effect

--- app/javascript/controllers/pictures_controller.js
+++ app/javascript/controllers/pictures_controller.js
@@ -2,9 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="pictures"
 export default class extends Controller {
-  static targets = ["thumbnail"];
+  static targets = ["thumbnail", "view"];
 
   connect() {
-    console.log(this.thumbnailTarget);
+    console.log(this.viewTarget);
+  }
+
+  update(e) {
+    console.log(e.target);
+    this.viewTarget.innerHTML = e.target.outerHTML;
+    this.viewTarget.classList.remove("thumbnail");
+    console.log(this.viewTarget);
   }
 }
